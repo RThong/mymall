64ba7cadf0dc98967c10bd046e792ebd94287e7f
RThong <403216075@qq.com>
2017-07-24 22:57:39 +0800

    first commit

---
+++ src/components/NavBread.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="nav-breadcrumb-wrap">
+		<div class="container">
+			<nav class="nav-breadcrumb">
+				<a href="/">Home</a>
+				<slot></slot><!-- 插槽 -->
+			</nav>
+		</div>
+	</div>    
+</template>
